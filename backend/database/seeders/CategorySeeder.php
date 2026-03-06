05fd767a92eaf35644d37211fe6b02f225a7aaf6
AgungDjalil <agungfirmansyahdjalil@gmail.com>
2026-03-06 14:07:03 +0700

    first commit

---
+++ backend/database/seeders/CategorySeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categories')->insert([
+            ['name' => 'Todo'],
+            ['name' => 'In Progress'],
+            ['name' => 'Review'],
+            ['name' => 'Done'],
+        ]);
+    }
+}
---
+++ backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+        $this->call([
+            CategorySeeder::class,
+        ]);
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123'),
+            'is_admin' => true
+        ]);
+        
+        User::factory()->count(10)->create();
+    }
+}
